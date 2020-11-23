3e42fa34650be0e7891679e2cd026af5f6f35902
awnicholls <awnicholls56@gmail.com>
2020-11-23 17:28:57 +0000

    fixed formatting on computerPlay in round

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let computerScore = 0;
 let playerScore = 0;
 let round = 0;
-function computerPlay ();
+function computerPlay () {
     const number = Math.floor(Math.random() * 3) + 1;
 switch (number) {
     case 1:
@@ -13,25 +13,26 @@ switch (number) {
     default:
         return  "paper";
 }
+}
 console.log(computerPlay)
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == 'rock' && computerSelection == 'Scissors') {
+    if (playerSelection == 'rock' && computerSelection == 'scissors') {
         playerScore++;
         return `You Win! Rock beats Scissors. Score: ${playerScore} to ${computerScore}`;
-    } else if (playerSelection == 'rock' && computerSelection == 'Paper') {
+    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
         computerScore++;
         return `You Lose! Paper beats rock. Score: ${playerScore} to ${computerScore}`;
-    } else if (playerSelection == 'paper' && computerSelection == 'Scissors') {
+    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
         computerScore++;
         return `You Lose! Scissors beat paper. Score: ${playerScore} to ${computerScore}`
-    } else if (playerSelection == 'paper' && computerSelection == 'Rock') {
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
         playerScore++;
         return `You Win! Paper beats rock. Score: ${playerScore} to ${computerScore}`
-    } else if (playerSelection == 'scissors' && computerSelection == "Rock") {
+    } else if (playerSelection == 'scissors' && computerSelection == "rock") {
         computerScore++;
         return `You Lose! Scissors beat rock. Score: ${playerScore} to ${computerScore}`
-    } else if (playerSelection == 'scissors' && computerSelection == 'Paper') {
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
         playerScore++;
         return `You Win! Scissors beat paper. Score: ${playerScore} to ${computerScore}`
     }
@@ -39,3 +40,6 @@ function playRound(playerSelection, computerSelection) {
         return `That was a draw. Score: ${playerScore} to ${computerScore}`;
 }
 
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
