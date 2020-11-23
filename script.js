11d6dc3e417ceb7fa7a51507abb8602088c0473e
awnicholls <awnicholls56@gmail.com>
2020-11-23 17:17:37 +0000

    added play round function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let computerScore = 0;
 let playerScore = 0;
 let round = 0;
-var computerPlay = Math.floor(Math.random() * 3) + 1;
+function computerPlay = Math.floor(Math.random() * 3) + 1;
 switch (computerPlay) {
     case 1:
         computerPlay = "rock";
@@ -13,3 +13,20 @@ switch (computerPlay) {
         computerPlay = "paper";
 }
 console.log(computerPlay)
+
+function playRound (computerSelection, playerSelection) {
+    case (computerSelection == "rock" && playerSelection == "paper"):
+    case (computerSelection == "paper" && playerSelection == "scissors"):
+    case (computerSelection == "scissors" && playerSelection == "rock"):
+        playerScore++;
+        return `You Win! ${playerSelection} beats ${computerSelection}. Score: ${playerScore} to ${computerScore}`;
+        break;
+    case (computerSelection == "rock" && playerSelection == "scissors"):
+    case (computerSelection == "paper" && playerSelection == "rock"):
+    case (computerSelection == "scissors" && playerSelection == "paper"):
+        computerScore++;
+        return `You Lose! ${playerSelection} beats ${computerSelection}. Score: ${playerScore} to ${computerScore}`;
+        break;
+    default:
+        `That was a draw. Try Again! Score: ${playerScore} to ${computerScore}`;    
+}
